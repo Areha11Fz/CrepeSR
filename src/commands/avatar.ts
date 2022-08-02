56ca9d4ce244b69de2d350ca287025eb598793c4
Areha11Fz <arifrh97z@gmail.com>
2022-08-02 19:47:21 +0700

    lint

--- src/commands/avatar.ts
+++ src/commands/avatar.ts
@@ -15,30 +15,35 @@ export default async function handle(command: Command) {
     const uid = Interface.target.player.db._id;
 
     switch (actionType) {
-        default:
+        default: {
             c.log(`Usage: /avatar <add|remove> <avatarId>`);
             break;
-        case "add":
+        }
+        case "add": {
             if (!avatarId) return c.log("No avatarId specified");
             // Check if it already exists
             const avatar = await Avatar.fromUID(uid, avatarId);
             if (avatar.length > 0) return c.log(`Avatar ${avatarId} already exists`);
             Avatar.create(uid, avatarId).then(a => c.log(`Avatar ${avatarId} added to ${a.ownerUid}`));
             break;
-        case "remove":
+        }
+        case "remove": {
             if (!avatarId) return c.log("No avatarId specified");
             Avatar.remove(uid, avatarId).then(() => c.log(`Avatar ${avatarId} removed from ${uid}`));
             break;
-        case "giveall":
+        }
+        case "giveall": {
             for (const id in AvatarExcelTable) {
                 Avatar.create(uid, parseInt(id)).then(() => c.log(`All Avatar has been added to ${uid}`));
             }
             break;
-        case "removeall":
+        }
+        case "removeall": {
             for (const id in AvatarExcelTable) {
                 Avatar.remove(uid, parseInt(id)).then(() => c.log(`All Avatar has been removed from ${uid}`));
             }
             break;
+        }
     }
 
     Interface.target.sync();
